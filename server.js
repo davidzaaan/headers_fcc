d6545d139e421d2e6ebc8d73fb1f9b4ce2965cf7
david <odmh2711@gmail.com>
2021-12-27 18:34:58 -0500

    Correcting typo

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.get("/api/hello", function (req, res) {
 // Challenge project
 app.get('/api/whoami', (req, res) => {
   res.json({
-    ipadress: req.headers.host,
+    ipaddress: req.headers.host,
     language: req.headers['accept-language'],
     software: req.headers['user-agent']
   })
